aaaf83c2adb5e4bb3f2e97a1312d7e058125c2c0
Benjamin <benjamin.guy.thomas@gmail.com>
2020-06-13 08:58:19 +0200

    90 done, but not using @ViewChild for template reference access

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  currentSection: string = "recipes";
+  currentSection: string = "shopping";
 
   registerSection(section: string) {
     this.currentSection = section;
--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Ingredient} from '../../shared/ingredient';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  addIngredient(nameIn: HTMLInputElement, amountIn: HTMLInputElement) {
+    const name = nameIn.value;
+    const amount = parseInt(amountIn.value, 10);
+    this.ingredientAdded.emit(new Ingredient(name, amount));
+    nameIn.value = '';
+    amountIn.value = '';
+  }
 }
