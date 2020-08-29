fe5f2483e20f6818a68a604240b9216f13d60783
Benjamin <benjamin.guy.thomas@gmail.com>
2020-08-29 06:27:15 +0200

    Add alert component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,11 +18,13 @@ import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 import {AuthComponent} from './auth/auth.component';
 import {LoadingSpinnerComponent} from './shared/loading-spinner/loading-spinner.component';
 import {AuthInterceptorService} from './auth/auth-interceptor.service';
+import {AlertComponent} from './shared/alert/alert.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoadingSpinnerComponent,
+    AlertComponent,
     HeaderComponent,
     AuthComponent,
     RecipesComponent,
