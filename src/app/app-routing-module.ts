209f42231f17df95d230b27427ed95de9dbb7dbd
Benjamin <benjamin.guy.thomas@gmail.com>
2020-08-30 09:01:47 +0200

    Up to 323

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -1,29 +1,12 @@
 import {RouterModule, Routes} from '@angular/router';
 import {NgModule} from '@angular/core';
-import {RecipesComponent} from './recipes/recipes.component';
 import {ShoppingListComponent} from './shopping-list/shopping-list.component';
-import {RecipeDetailComponent} from './recipes/recipe-detail/recipe-detail.component';
-import {MissingRecipeDetailComponent} from './recipes/missing-recipe-detail/missing-recipe-detail.component';
-import {RecipeEditComponent} from './recipes/recipe-edit/recipe-edit.component';
-import {RecipesResolverService} from './recipes/recipes-resolver.service';
 import {AuthComponent} from './auth/auth.component';
 import {NamedRoutes} from './named-routes';
-import {AuthGuard} from './auth/auth.guard';
 import {Auth2Component} from './auth/auth2.component';
 
 const routes: Routes = [
   {path: '', redirectTo: NamedRoutes.Recipes, pathMatch: 'full'},
-  {
-    path: NamedRoutes.Recipes.substr(1),
-    component: RecipesComponent,
-    canActivate: [AuthGuard],
-    children: [
-      {path: '', component: MissingRecipeDetailComponent},
-      {path: 'new', component: RecipeEditComponent},
-      {path: ':id', component: RecipeDetailComponent, resolve: [RecipesResolverService]},
-      {path: ':id/edit', component: RecipeEditComponent, resolve: [RecipesResolverService]},
-    ]
-  },
   {path: NamedRoutes.ShoppingList.substr(1), component: ShoppingListComponent},
   {path: NamedRoutes.Auth, component: AuthComponent},
   {path: 'auth2', component: Auth2Component},
--- src/app/recipes/recipes.module.ts
+++ src/app/recipes/recipes.module.ts
@@ -5,9 +5,9 @@ import {RecipeDetailComponent} from './recipe-detail/recipe-detail.component';
 import {RecipeItemComponent} from './recipe-list/recipe-item/recipe-item.component';
 import {MissingRecipeDetailComponent} from './missing-recipe-detail/missing-recipe-detail.component';
 import {RecipeEditComponent} from './recipe-edit/recipe-edit.component';
-import {RouterModule} from '@angular/router';
 import {CommonModule} from '@angular/common';
 import {ReactiveFormsModule} from '@angular/forms';
+import {RecipesRoutingModule} from './recipes-routing.module';
 
 const components = [
   RecipesComponent,
@@ -22,7 +22,7 @@ const components = [
   declarations: components,
   exports: components,
   imports: [
-    RouterModule,
+    RecipesRoutingModule,
     CommonModule,
     ReactiveFormsModule
   ]
