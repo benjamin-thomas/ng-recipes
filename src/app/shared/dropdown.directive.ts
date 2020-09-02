759a67f837d56b972170757a319fb18b558689d1
Benjamin <benjamin.guy.thomas@gmail.com>
2020-09-02 06:24:26 +0200

    Extra notes on dropdown directive

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -4,12 +4,29 @@ import {Directive, HostBinding, HostListener} from '@angular/core';
   selector: '[appDropdown]'
 })
 export class DropdownDirective {
+  showExample = false;
+
   // This adds/removes the class "open" if true/false
+  // HostBinding describes "to which property of the hosting element we want to bind"
+  // HostBinding parameter examples:
+  //  - style.backgroundColor <- we are accessing the DOM property which doesn't know dashes
+  //  - $1.style.backgroundColor <- minus $1 which is how you would inspect an element property after focus in chrome's devtools
   @HostBinding('class.open') private isOpen = false;
 
+  // I think this was the course's code, which I simplified
   // @HostBinding('class.open') get open() { return this.isOpen; }
+
+  @HostBinding('style.border') private border: string; // just an example
 
   @HostListener('click') toggleOpen() {
     this.isOpen = !this.isOpen;
+
+    if (this.showExample) {
+      if (this.isOpen) {
+        this.border = '1px solid red';
+      } else {
+        this.border = null;
+      }
+    }
   }
 }
